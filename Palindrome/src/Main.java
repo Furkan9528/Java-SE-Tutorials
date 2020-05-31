6dde92795d8f367cf7ee173e0815b66ac64dac68
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2020-05-31 10:57:12 +0200

    Palindrome
    Palindrome

---
+++ Palindrome/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Bir Cümlenin Palindrome olup olmadığını bulma
+        
+        // kasaylabalyasak
+		// taşat
+
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.println("Cümleyi giriniz :");
+		String cumle=scanner.nextLine();
+		
+		Stack<Character> stack= new Stack<Character>();
+		
+		for(int i=0; i<cumle.length()/2; i++) {
+			stack.push(cumle.charAt(i));
+		}
+		
+		
+		if(isPalindrome(cumle,stack)) {
+			 System.out.println("Bu cümle Palindromedur...");
+		}
+		
+		
+		 else {
+	            System.out.println("Bu cümle Palindrom değildir...");
+	        }
+
+		
+	}
+
+	private static boolean isPalindrome(String cumle, Stack<Character> stack) {
+		
+		for(int i=(cumle.length()/2)+1; i<cumle.length();  i++ ) {
+		
+			if(cumle.charAt(i) != stack.pop()) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+}
