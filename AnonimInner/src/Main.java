055a144be7e5fad0ddd65e9c6a942b2b6cb2ae47
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2020-06-09 22:27:39 +0200

    AnonimInner
    AnonimInner

---
+++ AnonimInner/src/Main.java
@@ -0,0 +1,50 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		IOgrenci ogrenci=new IOgrenci() {
+			
+			@Override
+			public void derse_gir() {
+				System.out.println("Ders çalışıyorum...");
+				
+			}
+			
+			@Override
+			public void ders_calis() {
+				System.out.println("Derse giriyorum...");
+				
+			}
+		};
+		ogrenci.ders_calis();
+		ogrenci.derse_gir();
+		
+		System.out.println("*************************************************************");
+		AOgrenci ogrenci2=new AOgrenci() {
+			
+			@Override
+			void kayit_yaptir() {
+				
+				System.out.println("Kayıt yapılıyor...");
+				
+			}
+		};
+		ogrenci2.kayit_yaptir();
+		ogrenci2.selamla();
+		
+	}
+	public static abstract class AOgrenci{ 
+		abstract void kayit_yaptir();
+		
+		public void selamla() {
+			System.out.println("Selamlar...");
+		}
+	}
+	
+	
+	public interface IOgrenci{
+		void ders_calis();
+		void derse_gir();
+	}
+	
+}
