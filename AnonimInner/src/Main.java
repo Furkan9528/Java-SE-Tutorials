abe32e810a26318d3849f55c4a3ec024b8d04dd8
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2020-08-15 21:02:56 +0200

    Update Main.java

--- AnonimInner/src/Main.java
+++ AnonimInner/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		IOgrenci ogrenci=new IOgrenci() {
 			
-			@Override
+			@Override 
 			public void derse_gir() {
 				System.out.println("Ders çalışıyorum...");
 				
